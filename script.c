c2c6b2bf2ccb38ae5de0e6c3dae5371065848815
MilaneUpdate <202502984383@alunos.estacio.br>
2025-06-15 19:25:31 -0300

    Trabalho Super Trunfo

--- script.c
+++ script.c
@@ -5,7 +5,7 @@ int main() {
     // = DADOS DA CARTA 1 =
     
     // Declaração das variáveis que representam os atributos da primeira carta
-    char carta1[10];                        // Nome ou identificação da carta (ainda não atribuído)
+    char carta1[10];                        // Nome ou identificação da carta (não foi atribuído nenhum valor)
     char estado1 = 'A';                     // Letra que representa o estado (exemplo: 'A' para Alagoas)
     char codigo1[5] = "A01";                // Código identificador da cidade
     char cidade1[20] = "Maceió";            // Nome da cidade
@@ -15,22 +15,23 @@ int main() {
     int nPontosTuristicos1 = 23;            // Número de pontos turísticos na cidade
 
     // Impressão dos dados da carta 1
-    printf("Carta 1: %s\n", carta1);  // Irá só repetir o nome (carta 1)(vai imprimir vazio)
-    printf("Estado: %c\n", estado1);
-    printf("Código: %s\n", codigo1);
-    printf("Cidade: %s\n", cidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.3f km²\n", area1);
-    printf("PIB: %.2f bilhões de reais\n", pib1);
-    printf("Número de Pontos Turísticos: %d\n", nPontosTuristicos1);
-    printf("\n");  // Linha em branco para separar visualmente as cartas
+    printf("Carta 1: %s\n", carta1);                                   // Irá só repetir o nome, sem atribuição, só para manter o formato
+    printf("Estado: %c\n", estado1);                                   // Letra do estado
+    printf("Código: %s\n", codigo1);                                   // Letra do estado seguida de número
+    printf("Cidade: %s\n", cidade1);                                   // Nome da cidade
+    printf("População: %d\n", populacao1);                             // População da cidade
+    printf("Área: %.3f km²\n", area1);                                 // Área com 3 casas decimais
+    printf("PIB: %.2f bilhões de reais\n", pib1);                      // PIB com 2 casas decimais
+    printf("Número de Pontos Turísticos: %d\n", nPontosTuristicos1);   // Números de pontos turísticos
+
+    printf("\n");                                                      // Linha em branco para criar um espaço entre as cartas
 
     // = DADOS DA CARTA 2 =
     
     // Declaração das variáveis para a segunda carta
-    char carta2[10];                        // Nome ou identificação da carta (ainda não atribuído)
-    char estado2 = 'C';                     // Letra que representa o estado (exemplo: 'C' para Ceará)
-    char codigo2[5] = "A02";                // Código identificador da cidade
+    char carta2[10];                        // Nome ou identificação da carta (não foi atribuído nenhum valor)
+    char estado2 = 'C';                     // Letra que representa o estado (exemplo: 'C' significa Ceará)
+    char codigo2[5] = "C02";                // Código identificador da cidade
     char cidade2[20] = "Fortaleza";         // Nome da cidade
     int populacao2 = 4284450;               // População da cidade
     float area2 = 312.353;                  // Área da cidade em km²
@@ -38,18 +39,19 @@ int main() {
     int nPontosTuristicos2 = 23;            // Número de pontos turísticos
 
     // Impressão dos dados da carta 2
-    printf("Carta 2: %s\n", carta2);       
-    printf("Estado: %c\n", estado2);
-    printf("Código: %s\n", codigo2);
-    printf("Cidade: %s\n", cidade2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %.3f km²\n", area2);      
-    printf("PIB: %.2f bilhões de reais\n", pib2); 
-    printf("Número de Pontos Turísticos: %d\n", nPontosTuristicos2);
+    
+    printf("Carta 2: %s\n", carta2);                                   // Irá só repetir o nome, sem atribuição, só para manter o formato
+    printf("Estado: %c\n", estado2);                                   // Letra do estado
+    printf("Código: %s\n", codigo2);                                   // Letra do estado seguida de número                 
+    printf("Cidade: %s\n", cidade2);                                   // Nome da cidade
+    printf("População: %d\n", populacao2);                             // População da cidade
+    printf("Área: %.3f km²\n", area2);                                 // Área com 3 casas decimais
+    printf("PIB: %.2f bilhões de reais\n", pib2);                      // PIB com 2 casas decimais
+    printf("Número de Pontos Turísticos: %d\n", nPontosTuristicos2);   // Números de pontos turísticos
 
-    printf("\n");  // Linha em branco final
+    printf("\n");  // Linha em branco para dar espaço a mensagem final
 
-    // Mensagem final
-    printf("Fim do programa!\n");
+    
+    printf("Fim do programa!\n");   // Mensagem final
 
     return 0;  // Retorno da função main indicando que o programa terminou com sucesso
